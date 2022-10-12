e324724844622d7516acdf0b31381f3b6fdbc841
b4rtaz <b4rtaz@gmail.com>
2022-10-12 13:06:23 +0200

    simple CI.

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -5,6 +5,7 @@ echo "auth"
 cd auth/functions
 npm install --loglevel error --no-progress --ignore-scripts
 npm run build
+npm run lint
 cd ../..
 
 echo "streams"
@@ -12,4 +13,6 @@ cd streams/functions
 npm install --loglevel error --no-progress --ignore-scripts
 npm run build
 npm run test
+npm run lint
+npm run format:check
 cd ../..
